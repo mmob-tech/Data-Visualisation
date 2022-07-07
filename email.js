f319cb74988594ef9577d52e1e45974847f53326
Luke Chester <68555055+TheCalmArtifact@users.noreply.github.com>
2022-07-07 22:09:50 +0100

    Update email.js

--- email.js
+++ email.js
@@ -24,53 +24,31 @@ let invalidDomains = [
   "protonmail.com",
 ];
 
-function emailCheck(event) {
+function validateEmail(event) {
   //Collecting user input and turning it to lower case
-  var email = document.getElementById("email").value;
-  let input = email.toLowerCase();
-  // stop refresh on button press
   event.preventDefault();
-  //email formatting
+  var email = document.getElementById("email").value;
+
+  if (!email) {
+    return console.error("Please enter a valid email");
+  }
+
+  const domain = email.toLowerCase().split("@")[1];
+
   const validRegex =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  //Splitting the email into two parts
-  let splitter = input.split("@");
-  //getting the part of the email that has domain name
-  let domain = splitter[1];
 
-  //Checking not an undefined value
-  if (typeof input !== undefined) {
-    //if input null - error
-    if (input === null) {
-      console.error("Please enter your email");
-      return;
-    }
-    //if no input - error
-    if (input == "") {
-      console.error("Please enter your email");
-      return;
-    }
-    //if text doesn't match regex, go through error
-    if (!input.match(validRegex)) {
-      console.error("Please enter a valid email");
-      return;
-    }
-    //if text does match regex, go through this if statement
-    if (input.match(validRegex)) {
-      //loops through the lists of domains and compares the one entered
-      for (let i = 0; i < invalidDomains.length; i++) {
-        //if there is a match return error
-        if (domain == invalidDomains[i]) {
-          console.error("Please enter a corporate email");
-        }
-      }
-      return;
-    }
-    //if input doesn't satisfy any of the if statements, return nothing - successful
-    else {
-      return;
+  // check standard email input
+  if (!email.match(validRegex)) {
+    return console.error("Please enter a valid email");
+  }
+
+  // check for forbidden domains
+  for (let invalidDomain of invalidDomains) {
+    if (domain === invalidDomain) {
+      return console.error("Please enter a corporate email");
     }
   }
-  console.error("Sorry, something went wrong");
-  return;
+
+  return console.log("Correct email entered");
 }
